96b74367cf6b71b94d936a7a3d301b03d18e7101
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-09-25 23:39:06 -0500

    Updates

---
+++ precache-manifest.51ec505e40288ed10c34072b22044ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434a293202bfa89db81521053a7c13f0",
+    "url": "/rick-y-morty/index.html"
+  },
+  {
+    "revision": "f5238ad30ba072d6a78f",
+    "url": "/rick-y-morty/static/css/main.bb78013d.chunk.css"
+  },
+  {
+    "revision": "b2d6c0f517fcd4d81612",
+    "url": "/rick-y-morty/static/js/2.1ac2cbba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rick-y-morty/static/js/2.1ac2cbba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5238ad30ba072d6a78f",
+    "url": "/rick-y-morty/static/js/main.188f5584.chunk.js"
+  },
+  {
+    "revision": "26081fd7b997da75f739",
+    "url": "/rick-y-morty/static/js/runtime-main.5031ae6f.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/rick-y-morty/static/media/logo.42d6efdb.png"
+  }
+]);
